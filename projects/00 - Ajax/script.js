993dae513f4d3deef0cbb63ea274460b45abea7b
senkasija <ivan.jevdjovic@gmail.com>
2020-08-05 21:41:27 +0200

    Ajax sa filtriranjem podataka.

--- projects/00 - Ajax/script.js
+++ projects/00 - Ajax/script.js
@@ -1,12 +1,42 @@
 /**
  * Objekti
  */
-let selectBox = document.getElementById("forma__select--kategorije");
-let selectBox1 = document.getElementById("forma__select--proizvodi");
+let kategorije = document.getElementById("forma__select--kategorije");
+let proizvodi = document.getElementById("forma__select--proizvodi");
+
+function kategorijeIzaberi() {
+
+  if(kategorije){
+
+    kategorije.setAttribute("disabled", true);
+
+    let getKategorije = new XMLHttpRequest();
+    getKategorije.open("GET", "https://admin.plodovisela.com/api/v1/categories");
+    getKategorije.send();
+
+    getKategorije.onload = function(){
+      
+      let odgovor = JSON.parse(getKategorije.response);
+      let odgovorZaKategorije = odgovor.categories;//ovo je niz kategorija
+
+      var ispisi = document.querySelector("#forma__select--kategorije");
+      //console.log(ispisi);
+
+      var selectSadrzaj = "";
+      for(i = 0; i < odgovorZaKategorije.length; i++) {
+        let optionSadzaj = "<option value=\""+odgovorZaKategorije[i].id+"\">" +odgovorZaKategorije[i].name+ "<option>";
+            selectSadrzaj += optionSadzaj;
+        }
+        kategorije.innerHTML = selectSadrzaj;
+        kategorije.removeAttribute("disabled");//treba ga  obrisati
+    }
+
+  } 
+}
 
 
 
-
+/*
 function kategorijeIzaberi() {
 
     if(selectBox){
@@ -16,14 +46,12 @@ function kategorijeIzaberi() {
       getKategorije.send();
 
       getKategorije.onload = function(){
-
         let odgovor = JSON.parse(getKategorije.response);
-        
         let odgovorZaKategorije = odgovor.categories;//ovo je niz kategorija
 
         var ispisi = document.querySelector("#forma__select--kategorije");
         console.log(ispisi);
-
+   
         odgovorZaKategorije.forEach(element => {
           textNode = document.createTextNode(element.name);
           liNode = document.createElement("option");
@@ -32,15 +60,48 @@ function kategorijeIzaberi() {
           });
       }
 
-    }
+    } 
 }
+      function proizvodiIzaberi() {
 
-function proizvodiIzaberi() {
+          if(selectBox1){
 
-    if(selectBox1){
+            let getProizvodi = new XMLHttpRequest();
+            getProizvodi.open("GET", "https://admin.plodovisela.com/api/v1/products?category=1");
+            getProizvodi.send();
 
+            getProizvodi.onload = function(){
+              let odgovor = JSON.parse(getProizvodi.response);
+              let odgovorZaProizvodi = odgovor.product;//ovo je niz kategorija
+
+              var ispisi = document.querySelector("#forma__select--proizvodi");
+              console.log(ispisi);
+
+              odgovorZaProizvodi.forEach(element => {
+                textNode = document.createTextNode(element.name);
+                liNode = document.createElement("option");
+                liNode.appendChild(textNode);
+                ispisi.appendChild(liNode);
+                });
+            }
+          }
+      }
+
+      */
+
+function proizvodiIzaberi(event) {
+
+  let odabranaKategorija = kategorije.options[kategorije.selectedIndex].value; 
+
+  console.log(odabranaKategorija);
+
+    if(proizvodi){
+       let url = "https://admin.plodovisela.com/api/v1/products";
+      if(odabranaKategorija > 0 ){
+        url = "https://admin.plodovisela.com/api/v1/products?category="+odabranaKategorija;
+      }
       let getProizvodi = new XMLHttpRequest();
-      getProizvodi.open("GET", "https://admin.plodovisela.com/api/v1/products?category=1");
+      getProizvodi.open("GET", url);
       getProizvodi.send();
 
       getProizvodi.onload = function(){
@@ -48,20 +109,22 @@ function proizvodiIzaberi() {
         let odgovorZaProizvodi = odgovor.product;//ovo je niz kategorija
 
         var ispisi = document.querySelector("#forma__select--proizvodi");
-        console.log(ispisi);
-
+        ispisi.innerHTML = ""; // uklanja prethodno pretraživanje
+       // console.log(ispisi);
+   
         odgovorZaProizvodi.forEach(element => {
+
           textNode = document.createTextNode(element.name);
           liNode = document.createElement("option");
           liNode.appendChild(textNode);
           ispisi.appendChild(liNode);
           });
       }
-    }
+
+    } 
 }
 
-let zaKlik = document.getElementById("forma__select--kategorije").options[0];
+kategorije.onload = kategorijeIzaberi();
 
-
-selectBox.onload = kategorijeIzaberi();
-selectBox1.onload = proizvodiIzaberi();
+kategorije.oninput = proizvodiIzaberi;
+//Objekat traži, ne funkciju i anonimna funkcija
